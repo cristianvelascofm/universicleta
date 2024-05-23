231e1e17b564fd02c983823eaf6026e2b26dd73e
cristianvelascofm <cfvelasco@unicauca.edu.co>
2024-05-23 16:40:45 -0500

    router defined main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainModuleComponent } from './components/main-module/main-module.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainModuleComponent
+
+  },
+  {
+    path: "**",
+    redirectTo: '/',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
